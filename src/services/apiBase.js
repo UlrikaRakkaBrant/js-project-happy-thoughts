01431309da5797ca60a5b8d8a2ea991905bbc567
Ulrika Einebrant <einebrantaren@gmail.com>
2025-12-09 19:27:02 +0100

    my own backend API

--- src/services/apiBase.js
+++ src/services/apiBase.js
@@ -1,3 +1,7 @@
+// Use your own backend API
+export const API_BASE = "https://js-project-api-j7vv.onrender.com";
+
 // Use Netlify env in prod, fall back to /api for local dev proxy
-export const API_BASE = "https://happy-thoughts-api-4ful.onrender.com";
+//export const API_BASE = "https://happy-thoughts-api-4ful.onrender.com";
+
 export const USE_MOCK = false; // toggle if you need offline dev
