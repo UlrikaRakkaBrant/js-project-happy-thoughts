b21a987473b55a13540a9ee7331fe10519396eed
Ulrika Einebrant <einebrantaren@gmail.com>
2025-12-09 19:55:47 +0100

    update src/main.jsx Wrap your app with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { AuthProvider } from "./hooks/useAuth.js";
+import "./index.css";
 
-import App from './App.jsx'
-
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
-)
+);
+
