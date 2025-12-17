5411ff437f3967d5e10f98e4d25d7c242daceaf2
Ulrika Einebrant <einebrantaren@gmail.com>
2025-12-17 18:36:47 +0100

    Clean code styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./hooks/useAuth";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,4 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </AuthProvider>
   </React.StrictMode>
 );
-
